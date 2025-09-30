d421530f1a646384bef608d8814efdf2c438e942
Nathan Honn <58925347+BobSanders64@users.noreply.github.com>
2025-09-29 22:06:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description=README.read_text(encoding="utf-8")
 
 setup(
     name="random_color_hex",
-    version="1.1.2",
+    version="2.0",
     author="Nathan Honn",
     author_email="randomhexman@gmail.com",
     description="Generate random CSS-style hex colors",
@@ -17,11 +17,35 @@ setup(
     license="Unlicense",
     license_files=("LICENSE.txt",),
     classifiers=[
+        #Language/OS
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: The Unlicense (Unlicense)",
         "Operating System :: OS Independent",
+
+        #Free-threading (PEP 703+)
+        "Programming Language :: Python :: Free Threading",
+        "Programming Language :: Python :: Free Threading :: 4 - Resilient",
+
+        #Maturity
+        "Development Status :: 5 - Production/Stable",
+
+        #Audiance
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+
+        #What it can be used for (topics)
+        "Topic :: Games/Entertainment",
+        "Topic :: Multimedia",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+        "Topic :: Scientific/Engineering",
+
+        #What its designed (but not required) to be used with (ecosystem)
+        "Framework :: Matplotlib",
     ],
     python_requires=">=3.11.0",
 )
-
