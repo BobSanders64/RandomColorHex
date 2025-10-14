a1d491ca1272645ffc1f5950daf74e52671d9c65
Nathan Honn <58925347+BobSanders64@users.noreply.github.com>
2025-10-14 00:41:13 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from .random_color_hex import RandomColorHex
 
-__version__="2.0.4"
+__version__="2.1"
 __author__="Nathan Honn"
 __email__="randomhexman@gmail.com"
 
 __all__=["RandomColorHex", "main", "BasicMain", "Credits", "Help"]
 
-def main(*, SuperLightColorsAllowed=True, SuperDarkColorsAllowed=True, HowDifferentShouldColorsBe='s'):
+def main(*, SuperLightColorsAllowed=True, SuperDarkColorsAllowed=True, HowDifferentShouldColorsBe='m'):
     return RandomColorHex().main(SuperLightColorsAllowed=SuperLightColorsAllowed, SuperDarkColorsAllowed=SuperDarkColorsAllowed, HowDifferentShouldColorsBe=HowDifferentShouldColorsBe)
 
 def BasicMain(*, SuperLightColorsAllowed=True, SuperDarkColorsAllowed=True):
@@ -17,5 +17,3 @@ def Credits():
 
 def Help():
     return RandomColorHex.Help()
-
-
