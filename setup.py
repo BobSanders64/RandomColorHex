4a1499ac3b2afe55388ddf3d1f395e53dba3add4
Nathan Honn <58925347+BobSanders64@users.noreply.github.com>
2025-10-14 00:58:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description=README.read_text(encoding="utf-8")
 
 setup(
     name="random_color_hex",
-    version="2.1.2",
+    version="2.1.3",
     author="Nathan Honn",
     author_email="randomhexman@gmail.com",
     description="Generate random CSS-style hex colors",
@@ -50,4 +50,5 @@ setup(
     ],
     python_requires=">=3.11.0",
 )
+
 
