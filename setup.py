4db006a390e6e55e594b5ea8170cef9308ebdd1c
Nathan Honn <58925347+NathanHonn@users.noreply.github.com>
2026-06-25 20:45:06 -0400

    Bump version from 3.0.4 to 3.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description=README.read_text(encoding="utf-8")
 
 setup(
     name="random_color_hex",
-    version="3.0.4",
+    version="3.0.5",
     author="Nathan Honn",
     author_email="randomhexman@gmail.com",
     description="The visually distinct, smartly separated, random color generator.",
